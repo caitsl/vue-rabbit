020d4f62c32057da3906bd885449f24c778f8dce
114514 <114514@hhh.com>
2024-11-25 20:54:50 +0800

    第一天代码:项目初始化，首页初步开发

---
+++ src/views/Home/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div style="height: 1200px;">
+     我是首页
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Layout/index.vue
@@ -0,0 +1,29 @@
+<script setup>
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
+import LayoutFix from './components/LayoutFix.vue'
+import {useCategoryStore} from '@/stores/category'
+import {onMounted} from 'vue'
+const categoryStore=useCategoryStore()
+onMounted(() => {
+  categoryStore.getCategory()
+})
+
+
+</script>
+
+<template>
+    <link rel="stylesheet" href="//at.alicdn.com/t/font_2143783_iq6z4ey5vu.css">
+    <LayoutFix/>
+    <LayoutNav />
+    <LayoutHeader />
+    <RouterView />
+    <LayoutFooter />
+</template>
+
+
+
+<style lang="scss" scoped>
+
+</style>
