c059d945aba2fe3bea25aa26f53c8cb9aa1d6f1a
114514 <114514@hhh.com>
2024-11-26 11:02:48 +0800

    Home 懒加载指令优化

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import '@/styles/common.scss'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { useIntersectionObserver } from '@vueuse/core'
+import { lazyPlugin } from '../directives'
 import App from './App.vue'
 import router from './router'
 
@@ -9,23 +9,7 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(lazyPlugin)
 
 
-app.directive('img-lazy',{
-
-   mounted(el,binding){
-      const { stop } = useIntersectionObserver(
-        el,
-        ([{isIntersecting}]) => {
-          console.log(isIntersecting);
-          if(isIntersecting){
-            el.src=binding.value
-            stop()
-          }
-        },
-      )
-   }
-
-
-})
 app.mount('#app')
