9e69bebc201c7a03114c1b45e76bb6f3b7e94887
114514 <114514@hhh.com>
2024-11-26 09:14:44 +0800

    home 整体结构拆分与分类实现

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,13 +1,20 @@
-<template>
-  <div style="height: 1200px;">
-     我是首页
-  </div>
-</template>
-
 <script setup>
-
+import HomeBanner from './components/HomeBanner.vue';
+import HomeCategory from './components/HomeCategory.vue';
+import HomeHot from './components/HomeHot.vue';
+import HomeNew from './components/HomeNew.vue';
+import HomeProduct from './components/HomeProduct.vue';
 </script>
 
-<style lang="scss" scoped>
+<template>
+     <div class="container">
+        <HomeCategory/>
+        <HomeBanner/>
+     </div>
+     <HomeNew/>
+     <HomeHot/>
+     <HomeProduct/>
+</template>
 
+<style lang="scss" scoped>
 </style>
