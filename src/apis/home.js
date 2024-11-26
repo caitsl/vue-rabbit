08d5575a915f939144c00ff1b487169cb42008b7
114514 <114514@hhh.com>
2024-11-26 10:06:02 +0800

    Home 人气推荐业务实现

--- src/apis/home.js
+++ src/apis/home.js
@@ -5,16 +5,31 @@ import http from '@/utils/http'
  * 获取轮播图数据
  * @returns
  */
-export function getBannerAPI(){
+export function getBannerAPI() {
   return http({
-      url:'/home/banner'
+    url: '/home/banner'
+  })
+}
+
+/**
+ * 获取新鲜好物数据
+ * @returns
+ */
+export function findNewAPI() {
+  return http({
+    url: '/home/new'
+  })
+}
+
+/**
+ * 获取人气推荐数据
+ * @returns
+ */
+export function findHotAPI(){
+  return http({
+      url:'/home/hot'
   })
 }
 
 
-export function findNewAPI(){
-  return http({
-    url:'/home/new'
-})
-}
 
