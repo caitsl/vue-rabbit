bd686f7e9bf6ef3ff3a7bdbd3bd353e4b7d2c2b2
114514 <114514@hhh.com>
2024-12-01 13:26:07 +0800

    自定义校验规则

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -5,6 +5,7 @@ import { ref } from "vue";
 const form = ref({
   account: "",
   password: "",
+  agree:true
 });
 
 //规则对象
@@ -14,6 +15,13 @@ const rules = ref({
     { required: true, message: "密码不能为空", trigger: "blur" },
     { min: 6, max: 14, message: "密码炜6-14个字符", trigger: "blur" },
   ],
+  agree:[
+     {
+        validator:(rule,val,callback)=>{
+            val?callback():new Error('请勾选协议')
+        }
+     }
+  ]
 });
 </script>
 
@@ -51,8 +59,8 @@ const rules = ref({
               <el-form-item prop="password" label="密码">
                 <el-input v-model="form.password"/>
               </el-form-item>
-              <el-form-item label-width="22px">
-                <el-checkbox size="large">
+              <el-form-item prop="agree" label-width="22px">
+                <el-checkbox v-model="form.agree"  size="large">
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
