15b3d183fe9176cf175431d28052bad099b65262
114514 <114514@hhh.com>
2024-12-01 18:08:17 +0800

    本地购物车-列表购物车数据渲染

--- src/router/index.js
+++ src/router/index.js
@@ -3,38 +3,42 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-      {
-          path:'/',
-          component:()=>import('@/views/Layout/index.vue'),
-          children:[
-              {
-                  path:'',
-                  component:()=>import('@/views/Home/index.vue')
-              },
-              {
-                path:'/category/:id',
-                component:()=>import('@/views/Category/index.vue')
-              },
-              {
-                path:'/category/sub/:id',
-                component:()=>import('@/views/Category/components/SubCategory.vue')
-              },
-              {
-                path:'/detail/:id',
-                component:()=>import('@/views/Detail/index.vue')
-              }
+    {
+      path: '/',
+      component: () => import('@/views/Layout/index.vue'),
+      children: [
+        {
+          path: '',
+          component: () => import('@/views/Home/index.vue')
+        },
+        {
+          path: '/category/:id',
+          component: () => import('@/views/Category/index.vue')
+        },
+        {
+          path: '/category/sub/:id',
+          component: () => import('@/views/Category/components/SubCategory.vue')
+        },
+        {
+          path: '/detail/:id',
+          component: () => import('@/views/Detail/index.vue')
+        },
+        {
+          path: '/cartlist',
+          component: () => import('@/views/CartList/index.vue')
+        }
 
-          ]
-      },
-      {
-        path:'/login',
-        component:()=>import('@/views/Login/index.vue')
+      ]
+    },
+    {
+      path: '/login',
+      component: () => import('@/views/Login/index.vue')
     }
   ],
-  scrollBehavior(){
-     return {
-        top:0
-     }
+  scrollBehavior() {
+    return {
+      top: 0
+    }
   }
 })
 
